8a364ac712be3abd7e920bfe384bdbeda331a6f5
yaplatonov <ya.platonov@gmail.com>
2023-11-08 17:30:44 +0300

    fit

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "yandex" {
-  service_account_key_file = "./tf_key.json"
+  service_account_key_file = "../../tmp/tf_key.json"
   folder_id                = local.folder_id
   zone                     = "ru-central1-a"
 }
@@ -26,9 +26,9 @@ resource "yandex_container_registry" "registry1" {
 }
 
 locals {
-  folder_id = "<INSERT YOUR FOLDER ID>"
+  folder_id = "b1gs0v2vqdts8glo467i"
   service-accounts = toset([
-    "catgpt-sa",
+    "yy-noted-catgpt-sa",
   ])
   catgpt-sa-roles = toset([
     "container-registry.images.puller",
@@ -42,7 +42,7 @@ resource "yandex_iam_service_account" "service-accounts" {
 resource "yandex_resourcemanager_folder_iam_member" "catgpt-roles" {
   for_each  = local.catgpt-sa-roles
   folder_id = local.folder_id
-  member    = "serviceAccount:${yandex_iam_service_account.service-accounts["catgpt-sa"].id}"
+  member    = "serviceAccount:${yandex_iam_service_account.service-accounts["yy-noted-catgpt-sa"].id}"
   role      = each.key
 }
 
@@ -51,7 +51,7 @@ data "yandex_compute_image" "coi" {
 }
 resource "yandex_compute_instance" "catgpt-1" {
     platform_id        = "standard-v2"
-    service_account_id = yandex_iam_service_account.service-accounts["catgpt-sa"].id
+    service_account_id = yandex_iam_service_account.service-accounts["yy-noted-catgpt-sa"].id
     resources {
       cores         = 2
       memory        = 1
@@ -73,8 +73,64 @@ resource "yandex_compute_instance" "catgpt-1" {
     }
     metadata = {
       docker-compose = file("${path.module}/docker-compose.yaml")
-      ssh-keys  = "ubuntu:${file("~/.ssh/devops_training.pub")}"
+      ssh-keys  = "ubuntu:${file("~/.ssh/github_yapl.pub")}"
     }
 }
 
+resource "yandex_compute_instance_group" "catgpt-group" {
+  name                = "catgpt-group1"
+  folder_id           = "${data.yandex_resourcemanager_folder.test_folder.id}"
+  service_account_id  = yandex_iam_service_account.service-accounts["yy-noted-catgpt-sa"].id
+  deletion_protection = true
+  instance_template {
+    platform_id = "standard-v1"
+    resources {
+      memory = 2
+      cores  = 2
+    }
+    boot_disk {
+      mode = "READ_WRITE"
+      initialize_params {
+        image_id = "${data.yandex_compute_image.ubuntu.id}"
+        size     = 4
+      }
+    }
+    network_interface {
+      network_id = "${yandex_vpc_network.my-inst-group-network.id}"
+      subnet_ids = ["${yandex_vpc_subnet.my-inst-group-subnet.id}"]
+    }
+    labels = {
+      label1 = "label1-value"
+      label2 = "label2-value"
+    }
+    metadata = {
+      foo      = "bar"
+      ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+    }
+    network_settings {
+      type = "STANDARD"
+    }
+  }
 
+  variables = {
+    test_key1 = "test_value1"
+    test_key2 = "test_value2"
+  }
+
+  scale_policy {
+    fixed_scale {
+      size = 3
+    }
+  }
+
+  allocation_policy {
+    zones = ["ru-central1-a"]
+  }
+
+  deploy_policy {
+    max_unavailable = 2
+    max_creating    = 2
+    max_expansion   = 2
+    max_deleting    = 2
+  }
+}
